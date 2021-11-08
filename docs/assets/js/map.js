12394a4197b94df449e3fff4c8636633147975c0
Alessandro <9991341+alessandrosp@users.noreply.github.com>
2021-11-09 08:41:03 +0900

    Update map.js

--- docs/assets/js/map.js
+++ docs/assets/js/map.js
@@ -2,6 +2,19 @@
 layout: blank
 ---
 {% assign foo = "info-i_maps.png" %}
+
+fetch("https://world.openfoodfacts.org/category/pastas/1.json").then(response => {
+    // indicates whether the response is successful (status code 200-299) or not
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${reponse.status}`)
+    }
+    return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => console.log(error))
+
 let map;
 
 function initMap() {
